6c1553404e8e399a754465f475a2d80e6be4722a
hdinhof1 <hdinhof1@binghamton.edu>
2016-06-20 10:17:13 -0400

    All tests pass

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,23 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackPlayer.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISBlackjackPlayer *newPlaya = [[FISBlackjackPlayer alloc] initWithName:@"Hank"];
+    FISCardDeck *deck = [[FISCardDeck alloc] init];
+    FISCard *card = [deck drawNextCard];
+    [newPlaya acceptCard:card];
+    NSLog(@"%@", newPlaya);
+    card = [deck drawNextCard];
+    [newPlaya acceptCard:card];
+    NSLog(@"%@", newPlaya);
+
     return YES;
 }
 
